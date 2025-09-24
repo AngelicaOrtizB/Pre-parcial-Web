5fc3bab474422e8de36c8b1ea506664e8ce75972
AngelicaOrtizB <a.ortizb2@uniandes.edu.co>
2025-09-24 09:26:23 -0500

    commit

--- src/app/types/book.ts
+++ src/app/types/book.ts
@@ -1,12 +1,3 @@
-import { Author } from "next/dist/lib/metadata/types/metadata-types";
-
-export interface Book {
-  id: number;
-  name: string;
-  isbn: string;
-  image: string;
-  publishingDate: string;
-  description: string;
-  reviews: string;
-  authors: Author;
-}
+import { Autor} from "./author";
+ export interface Book 
+ { id: number; name: string; isbn: string; image: string; publishingDate: string; description: string; reviews: string; authors: Autor; }
